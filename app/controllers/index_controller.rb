f42af4fee5cb2540b4acd9faf8f4951efda9c7a2
universtounding <kendmorse@gmail.com>
2018-12-26 14:37:45 -0500

    This adds new routes for blog and index pages

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,12 @@
+class IndexController < ApplicationController
+  def home
+
+  end
+  def about
+
+  end
+  def contact
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "index/home", to: "index#home"
+  get "index/about", to: "index#about"
+  get "index/contact", to: "index#contact"
+  get "articles/new", to: "articles#new"
 end
